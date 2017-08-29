a233148a8755b037e809e37ec0c6ebc75c4dd037
kenjataimu <tkazubek@gmail.com>
2017-08-29 18:47:13 +0200

    HackAssembler::assembly method. bin/assembler program.

--- lib/hack_assembler.rb
+++ lib/hack_assembler.rb
@@ -1,6 +1,11 @@
-$LOAD_PATH.unshift File.join(File.dirname(__FILE__), "hack_assembler")
+$LOAD_PATH.unshift File.dirname(__FILE__)
 
-require "parser"
+require "hack_assembler/parser"
 
 module HackAssembler
+  def self.assembly
+    Parser.new(STDIN).instructions.each do |instruction|
+      STDOUT.puts instruction.to_binary
+    end
+  end
 end
