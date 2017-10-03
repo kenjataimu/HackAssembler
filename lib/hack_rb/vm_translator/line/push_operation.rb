46d29e97f80ac513bc51fb559b2fe8f34aa50ef7
TKazubek <tkazubek@gmail.com>
2017-10-03 21:28:40 +0200

    push constant/static/temp

--- lib/hack_rb/vm_translator/line/push_operation.rb
+++ lib/hack_rb/vm_translator/line/push_operation.rb
@@ -10,10 +10,11 @@ module HackRB
         "that" => "THAT"
       }
 
-      def initialize(body, number)
-        super
+      def initialize(body, number, filename)
+        super(body, number)
         @segment, @offset = self.class.regexp.match(body).captures
-        @segment = SEGMENTS[@segment]
+        @segment = SEGMENTS[@segment] || @segment
+        @filename = filename.capitalize
       end
 
       def self.regexp
@@ -25,6 +26,24 @@ module HackRB
         when "constant"
           %W[
             @#{@offset}
+            D=A
+            @SP
+            M=M+1
+            A=M-1
+            M=D
+          ]
+        when "static"
+          %W[
+            @#{filename}.#{@offset}
+            D=M
+            @SP
+            M=M+1
+            A=M-1
+            M=D
+          ]
+        when "temp"
+          %W[
+            @#{5 + @offset.to_i}
             D=M
             @SP
             M=M+1
