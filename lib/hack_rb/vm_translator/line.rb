7bac1dccc48abe01b59f9aad194e1d8332debdca
kenjataimu <tkazubek@gmail.com>
2017-10-04 14:54:01 +0200

    Parser all instruction types.

--- lib/hack_rb/vm_translator/line.rb
+++ lib/hack_rb/vm_translator/line.rb
@@ -9,6 +9,9 @@ module HackRB
       def self.parse(vm_line)
         case vm_line
         when /(add|sub|eq|gt|lt|and|or|)/ then BinaryOperation.new(vm_line)
+        when /(neg|not|)/ then UnaryOperation.new(vm_line)
+        when /push .+/ then PushOperation.new(vm_line)
+        when /pop .+/ then PopOperation.new(vm_line)
         end
       end
     end
@@ -16,3 +19,6 @@ module HackRB
 end
 
 require "hack_rb/vm_translator/line/binary_operation"
+require "hack_rb/vm_translator/line/unary_operation"
+require "hack_rb/vm_translator/line/push_operation"
+require "hack_rb/vm_translator/line/pop_operation"
